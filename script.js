638e39795506fab0b3c4cbbc959ee013a8b8e3dd
Taimoor <taimoorsaleem2005@gmail.com>
2026-04-17 20:25:45 +0500

    -/+ op

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const btns = document.querySelectorAll(".btn");
 const input = document.querySelector("#val");
 
-let prevDigit = null; let result = null ; let currDigit = null
-let digit = [] 
+let prevDigit = null; let result = null; let currDigit = null
+let digit = []; let lastOp = -1
 
 btns.forEach((btn) => {
     btn.addEventListener("click", (e) => {
@@ -20,18 +20,29 @@ btns.forEach((btn) => {
                 return
 
             }
-            else if(value == "+/−") {
+            else if (value == "+/−") {
 
-                let curr = input.textContent
-                if(curr == "0" || curr == "") return
+                let num = digit.join("")
 
-                let negated = (parseFloat(curr) * -1).toString()
-                
-                digit = [negated]
-                input.textContent = negated
-                
+                if (num.length == 1 || num.length == 2) {
+                    if (num.startsWith("-")) {
+                        num = "+" + (num.slice(1))
+                    } else {
+                        num = "-" + num
+                    }
+                } else {
+                    num = num.slice(0, num.length - 2) + (
+                        num.slice(num.length - 2).startsWith("+")
+                            ? `-${num.slice(num.length - 1)}`
+                            : `+${num.slice(num.length - 1)}`
+                    );
+                }
+
+                digit = []
+                digit.push(num)
+                input.textContent = num
+
             }
-            
             else if (value == "=") {
 
                 let finalExpression = digit.join("")
@@ -47,22 +58,20 @@ btns.forEach((btn) => {
 
             }
             else {
-                if (prevDigit != null) {
-                    if(isNaN(value)){
-                        digit.push(value)
-                    }else{
-                        input.textContent = ""
-                    }
-
-                    prevDigit = null
+                if (prevDigit != null && isNaN(value)) {
+                    digit.push(prevDigit.toString())
+                } else if (prevDigit != null & !isNaN(value)) {
+                    digit = []
+                    input.textContent = ""
                 }
 
+                prevDigit = null
                 digit.push(value)
-                
+
                 input.textContent = digit.join("")
             }
         }
-        catch(err) {
+        catch (err) {
             input.textContent = "Math Error"
             digit = []
         }
